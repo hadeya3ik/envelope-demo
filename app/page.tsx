627dd00015bd58033b9fa00ce7f8543ef3904891
Hadeya Ikram <hadeyaik@gmail.com>
2025-12-12 00:22:29 -0500

    build

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,91 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import gsap from "gsap";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { useRef, useEffect, useState } from "react"
+
+gsap.registerPlugin(MorphSVGPlugin);
+
+export default function Home() { 
+  const [isOpen, setIsOpen] = useState(false)
+  const tl = useRef<gsap.core.Timeline | null>(null);
+
+  useEffect(() => {
+    tl.current = gsap.timeline({ paused: true });
+
+    tl.current
+    .addLabel("closed")
+
+    .to("#envelopeTopPath", {
+      duration: 0.8,
+      morphSVG: "#envelopeTopPeakPath",
+      ease: "power2.inOut",
+    })
+    .addLabel("peak")
+    
+    .to("#envelopeTopPath", {
+      duration: 0.8,
+      morphSVG: {
+        shape: "#envelopeTopOpenPath",
+        shapeIndex: 0,
+      },
+      ease: "power2.inOut",
+    })
+    
+    .addLabel("open");
+
+    
+    
+  }, [])
+  
+  useEffect(() => {
+    console.log(isOpen)
+  })
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="w-full h-full flex items-center justify-center" >
+      <div 
+      onMouseEnter={(() => { tl.current?.tweenTo("peak") })} 
+      onMouseLeave={() => { tl.current?.tweenTo("closed") }} 
+      onClick={() => { 
+        if (isOpen) {tl.current?.tweenTo("closed"); setIsOpen(false) } 
+        else {tl.current?.tweenTo("open"); setIsOpen(true) };  }}
+
+
+      className="relative w-191 h-170 " id="container">
+
+        <svg className="absolute z-100 bottom-0" id="envelope-top" width="764" height="679" viewBox="0 0 764 679" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path id="envelopeTopPath" d="M0.5 274V325.775L381.75 600L763 325.775V274H381.75H0.5Z" fill="white" stroke="black"/>
+        </svg>
+
+        <svg className="absolute hidden z-100" id="envelope-top-peak" width="764" height="679" viewBox="0 0 764 679" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path id="envelopeTopPeakPath" d="M0.5 274V325.775L381.75 495.5L763 325.775V274H381.75H0.5Z" fill="white" stroke="black"/>
+        </svg>
+
+        <svg className="absolute hidden z-10 bottom-0" id="envelope-top-opened" width="764" height="679" viewBox="0 0 764 679" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path id="envelopeTopOpenPath" d="M0.5 274.5V222.725L381.75 53L763 222.725V274.5H381.75H0.5Z" fill="white" stroke="black"/>
+        </svg>
+
+        <div className="bg-amber-100 absolute z-5 right-[20%] bottom-[20%] h-[200px] w-[500px]" > 
+          <h1>hello</h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        
+        <svg className="absolute z-10 bottom-0" id="envelope-base"
+         width="764" height="360" viewBox="0 0 764 360" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.5 0.906006V359.406L327.5 206.921L0.5 0.906006Z" fill="white"/>
+          <path d="M763 0.906006V359.406L436 206.921L763 0.906006Z" fill="white"/>
+          <path d="M327.5 206.921L0.5 359.406H381.75H763L436 206.921C366.5 206.921 397 206.921 327.5 206.921Z" fill="white"/>
+          <path d="M0.5 359.406V0.906006L327.5 206.921M0.5 359.406L327.5 206.921M0.5 359.406H381.75H763M327.5 206.921C397 206.921 366.5 206.921 436 206.921M763 359.406V0.906006L436 206.921M763 359.406L436 206.921" stroke="black"/>
+        </svg>
+
+        <svg className="absolute z-0 bottom-0" id="envelope-back" width="764" height="406" viewBox="0 0 764 406" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M763 404.775V0.5L394.5 0.500031L0.5 0.5V404.775H394.5H763Z" fill="white"/>
+          <path d="M0.5 404.775V0.5L394.5 0.500031L763 0.5V404.775H394.5H0.5Z" fill="white"/>
+          <path d="M763 404.775V0.5L394.5 0.500031L0.5 0.5V404.775H394.5H763Z" stroke="black"/>
+        </svg>
+
+
+      </div>
     </div>
   );
 }
